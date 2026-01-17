dc69092a9a83fcd168d5d6d70f46853a4ba80d6f
TPFTMON <tpftmoon@gmail.com>
2026-01-17 16:44:19 +0100

    ex02 is done

---
+++ ex02/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(void)
+{
+    std::string brain_string =  "HI THIS IS BRAIN";
+    std::string *stringPTR = &brain_string;
+    std::string &stringREF = brain_string;
+
+    std::cout << "The memory address of the string variable: " << &brain_string << "\n";
+    std::cout << "The memory address held by stringPTR:      " << stringPTR << "\n";
+    std::cout << "The memory address held by stringREF:      " << &stringREF << "\n\n";
+
+    std::cout << "The value of the string variable:  " << brain_string << "\n";
+    std::cout << "The value pointed to by stringPTR: " << *stringPTR << "\n";
+    std::cout << "The value pointed to by stringREF: " << stringREF << "\n";
+
+    return (0);
+}
